3051fd2031523b41d342710f225ec89269ccdf8b
Vinicius Louzada Jacobsem <vinicius.lj@gmail.com>
2022-08-10 16:40:00 -0300

    initial commit

---
+++ src/main/java/viniciuslj/vote/api/VoteApiApplication.java
@@ -0,0 +1,13 @@
+package viniciuslj.vote.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/viniciuslj/vote/api/VoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package viniciuslj.vote.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
