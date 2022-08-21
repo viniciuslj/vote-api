4b8f5a45b36cdbc9270747be97174e26aba27faf
Vinicius Louzada Jacobsem <vinicius.lj@gmail.com>
2022-08-21 01:02:15 -0300

    cut: exclusão da entidade member

--- src/main/java/viniciuslj/vote/api/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package viniciuslj.vote.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import viniciuslj.vote.api.domain.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
