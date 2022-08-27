a58133b981fb5cb1de280c7435718406c275b4e6
Vinicius Louzada Jacobsem <vinicius.lj@gmail.com>
2022-08-26 22:25:26 -0300

    build: dependência e configuração do open feign

--- src/main/java/viniciuslj/vote/api/VoteApiApplication.java
+++ src/main/java/viniciuslj/vote/api/VoteApiApplication.java
@@ -2,6 +2,7 @@ package viniciuslj.vote.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableScheduling
 @EnableWebMvc
+@EnableFeignClients
 public class VoteApiApplication {
 
 	public static void main(String[] args) {
