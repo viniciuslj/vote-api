2069a21d8af1eb51b4439348bd0cd5b4549edf3b
Vinicius Louzada Jacobsem <vinicius.lj@gmail.com>
2022-08-26 22:59:12 -0300

    fix: correção de falha na desserialização do json retornado pela api de validação de cpf

--- src/main/java/viniciuslj/vote/api/vendor/services/user/UserInformation.java
+++ src/main/java/viniciuslj/vote/api/vendor/services/user/UserInformation.java
@@ -1,9 +1,13 @@
 package viniciuslj.vote.api.vendor.services.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class UserInformation {
     public static class STATUS {
