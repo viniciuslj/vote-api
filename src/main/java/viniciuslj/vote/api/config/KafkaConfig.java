cf23f92e1312194545b7857f760e5fc57c893cfa
Vinicius Louzada Jacobsem <vinicius.lj@gmail.com>
2022-08-29 15:13:47 -0300

    ci: integração com circleci e configuração dos profiles test, dev e prod

--- src/main/java/viniciuslj/vote/api/config/KafkaConfig.java
+++ src/main/java/viniciuslj/vote/api/config/KafkaConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -17,6 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@ConditionalOnProperty(value = "kafka.enable", havingValue = "true", matchIfMissing = true)
 public class KafkaConfig {
 
     @Value(value = "${kafka.bootstrapAddress}")
